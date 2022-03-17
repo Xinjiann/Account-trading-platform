ab928bcf95d4100d7266526432ab10cbc57b42ed
Zheyuan Tian <2527715T@student.gla.ac.uk>
2022-03-17 17:25:02 +0000

    register complete

--- rango/models.py
+++ rango/models.py
@@ -38,7 +38,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
+    
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
